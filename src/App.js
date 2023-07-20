4aa574b6e9e8a2b8b86ec31ee45a29f65a5af83a
IceRed0991 <wernergutschow@hotmail.com>
2023-07-20 17:53:47 -0300

    Refatorando Code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
+import { MdDelete } from "react-icons/md";
+
 import "./App.css";
 import "./index.css";
 
 const App = () => {
-  const [value, setValue] = useState("");
   const ESCAPE_KEY = 27;
   const ENTER_KEY = 13;
+
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
 
   const erase = () => {
     setValue("");
@@ -13,6 +17,16 @@ const App = () => {
 
   const submit = () => {
     console.log("submit -", value);
+
+    setTodos([
+      ...todos,
+      {
+        id: new Date().getTime(),
+        title: value,
+        checked: false,
+      },
+    ]);
+
     erase();
   };
   const onChange = (event) => {
@@ -25,6 +39,18 @@ const App = () => {
     } else if (event.which === ESCAPE_KEY) {
       erase();
     }
+  };
+
+  const onToggle = (todo) => {
+    setTodos(
+      todos.map((obj) =>
+        obj.id === todo.id ? { ...obj, checked: !todo.checked } : obj
+      )
+    );
+  };
+
+  const onRemove = (todo) => {
+    setTodos(todos.filter((obj) => obj.id !== todo.id));
   };
 
   return (
@@ -40,6 +66,28 @@ const App = () => {
           onChange={onChange}
           onKeyDown={onKeyDown}
         />
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id.toString()}>
+              <span
+                className={["todo", todo.checked ? "checked" : ""].join(" ")}
+                onClick={() => onToggle(todo)}
+                onKeyPress={() => onToggle(todo)}
+                role="button"
+                tabIndex={0}
+              >
+                {todo.title}
+              </span>
+              <button
+                className="remove"
+                type="button"
+                onClick={() => onRemove(todo)}
+              >
+                <MdDelete size={28} />
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
     </section>
   );
