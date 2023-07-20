8bc2b30a200c9dfda5350aadfc4fde4a70aa343e
IceRed0991 <wernergutschow@hotmail.com>
2023-07-19 21:07:11 -0300

    Criando Projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState("");
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const erase = () => {
+    setValue("");
+  };
+
+  const submit = () => {
+    console.log("submit -", value);
+    erase();
+  };
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+    } else if (event.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section id="app" className="container">
+      <header>
+        <h1 className="title">To-Do</h1>
       </header>
-    </div>
+      <section className="main">
+        <input
+          className="new-todo"
+          placeholder="o que precisa ser feito?"
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+      </section>
+    </section>
   );
-}
+};
 
 export default App;
